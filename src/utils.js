7cf89a2630846f814ef6ee5e8db53fb8a7ffab4a
dwqs <461147874@qq.com>
2018-09-28 18:55:50 +0800

    refactor: Add some utils

---
+++ src/utils.js
@@ -0,0 +1,32 @@
+export const noop = () => {}
+
+export const isSupportPassive = () => {
+  let supportsPassive = false
+  try {
+    const opts = {}
+    Object.defineProperty(opts, 'passive', {
+      get () {
+        supportsPassive = true
+      }
+    })
+    window.addEventListener('test-passive', null, opts)
+  } catch (e) {}
+
+  return supportsPassive
+}
+
+export const requestAnimationFrame =
+  window.requestAnimationFrame ||
+  window.webkitRequestAnimationFrame ||
+  window.mozRequestAnimationFrame ||
+  window.msRequestAnimationFrame ||
+  function (cb) {
+    window.setTimeout(cb, 1000 / 60)
+  }
+
+export const cancelAnimationFrame =
+  window.cancelAnimationFrame ||
+  window.mozCancelAnimationFrame ||
+  function (id) {
+    window.clearTimeout(id)
+  }
